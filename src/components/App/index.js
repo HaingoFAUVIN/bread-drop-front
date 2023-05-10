80df3c4c38494c781f99c1c157f7e8f49d67f825
Yannick-Dupuy <yannick.dupuypro@gmail.com>
2023-05-10 15:43:38 +0200

    Création des pages Inscription, Connexion et Inscription Pro, en responsive desktop et mobile

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,27 +1,23 @@
 import './styles.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
-import  Home from '../Home/Home';
-import  Login from '../User/Login/Login';
-import  CheckIn from '../User/CheckIn/CheckIn';
-import  CheckInPro from '../User/CheckInPro/CheckInPro';
-import  Bakery from '../Bakery/Bakery';
+import Home from '../Home/Home';
+import Login from '../User/Login/Login';
+import CheckIn from '../User/CheckIn/CheckIn';
+import CheckInPro from '../User/CheckInPro/CheckInPro';
+import Bakery from '../Bakery/Bakery';
 import { Routes, Route } from 'react-router-dom';
-
-
 
 function App() {
   return (
-    <>
     <Router>
       <Routes>
-        <Route path="/inscription" element={<Login />} />
-        <Route path="/connexion" element={<CheckIn />} />
-        <Route path="/connexion-pro" element={<CheckInPro />} />
+        <Route path="/inscription" element={<CheckIn />} />
+        <Route path="/connexion" element={<Login />} />
+        <Route path="/inscription-pro" element={<CheckInPro />} />
         <Route path="/home" element={<Home />} />
         <Route path="/boulangerie-produit" element={<Bakery />} />
       </Routes>
     </Router>
-    </>
   );
 }
 
