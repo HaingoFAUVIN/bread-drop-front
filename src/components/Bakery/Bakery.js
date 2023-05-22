4694416de3376fc73c943f43dec2c2422c3d9d58
BukaJunior <juniorbuka@gmail.com>
2023-05-22 11:46:50 +0200

    Redirection d'une URL dynamique vers 404 si elle n'existe pas, mise en place d'un composant CircularIndeterminate (loader) + scss loader

--- src/components/Bakery/Bakery.js
+++ src/components/Bakery/Bakery.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
@@ -12,6 +13,8 @@ import Bread from './Bread/Bread';
 import Pastry from './Pastry/Pastry';
 import Viennoiserie from './Viennoiserie/Viennoiserie';
 import Sandwich from './Sandwich/Sandwich';
+import NotFound from '../NotFound/NotFound';
+import CircularIndeterminate from '../CircularIndeterminate';
 
 // DATA, SCSS, ASSETS
 
@@ -32,6 +35,7 @@ function Bakery({
   const [viennoiseries, setViennoiseries] = useState([]);
   const [breads, setBreads] = useState([]);
   const [sandwiches, setSandwiches] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const api = axios.create({
     baseURL: 'http://davyvistel-server.eddi.cloud/',
@@ -42,6 +46,7 @@ function Bakery({
 
   useEffect(() => {
     const fetchBakery = async () => {
+      setIsLoading(true);
       try {
         const response = await api.get(`api/boulangeries/${id}`);
         console.log(response.data);
@@ -70,25 +75,31 @@ function Bakery({
       catch (erreur) {
         console.log(erreur);
       }
+      setIsLoading(false);
     };
     fetchBakery();
   }, [id]);
 
   return (
-    <>
-      {/* SUPPRIMER LA NAV AVANT DE PULL */}
-      <Nav />
+    isLoading ? <CircularIndeterminate />
+      : bakery === null
+        ? <NotFound />
+        : (
+          <>
+            {/* SUPPRIMER LA NAV AVANT DE PULL */}
+            <Nav />
 
-      <BakeryBanner />
-      <BakerySearchProducts />
-      <Bread isVisible={isVisible} setIsVisible={setIsVisible} breads={breads} />
-      <Pastry isVisible2={isVisible2} setIsVisible2={setIsVisible2} pastries={pastries} />
-      <Viennoiserie isVisible3={isVisible3} setIsVisible3={setIsVisible3} viennoiseries={viennoiseries} />
-      <Sandwich isVisible4={isVisible4} setIsVisible4={setIsVisible4} sandwiches={sandwiches} />
+            <BakeryBanner />
+            <BakerySearchProducts />
+            <Bread isVisible={isVisible} setIsVisible={setIsVisible} breads={breads} />
+            <Pastry isVisible2={isVisible2} setIsVisible2={setIsVisible2} pastries={pastries} />
+            <Viennoiserie isVisible3={isVisible3} setIsVisible3={setIsVisible3} viennoiseries={viennoiseries} />
+            <Sandwich isVisible4={isVisible4} setIsVisible4={setIsVisible4} sandwiches={sandwiches} />
 
-      {/* SUPPRIMER LE FOOTER AVANT DE PULL */}
-      <Footer />
-    </>
+            {/* SUPPRIMER LE FOOTER AVANT DE PULL */}
+            <Footer />
+          </>
+        )
   );
 }
 
